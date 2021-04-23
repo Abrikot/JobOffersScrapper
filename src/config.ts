dd39a5a83258ef2fbca2b47c0b165278206c8c83
Maxence Cornaton <cornatonmaxence@gmail.com>
2021-04-23 17:13:03 +0200

    Moving config to json file

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,22 @@
+import * as fs from 'fs';
+
 import { Website } from "./model/website";
 import { Apec } from "./websites/apec";
 import { LinkedIn } from "./websites/linkedin";
 import { WelcomeToTheJungle } from "./websites/wttj";
 
-export const query = 'Développeur';
-export const resultFile = 'offers.html';
 export const ignoredCompaniesFile = 'resources/ignoredCompanies.json';
-export const maxOffersPerCompany = 10;
 
-export const websites : Website[] = [
+export const websites: Website[] = [
     new Apec(),
     new WelcomeToTheJungle(),
     new LinkedIn()
 ];
+
+console.log('Loading config');
+const configFilePath = 'resources/config.json';
+export const {
+    query,
+    resultFile,
+    maxOffersPerCompany,
+} = JSON.parse(fs.readFileSync(configFilePath, 'utf-8') || '{}');
